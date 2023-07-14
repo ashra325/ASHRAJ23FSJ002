950d2294b6db6bda2f8e6abca0f634318d56d126
ashra325 <86569956+ashra325@users.noreply.github.com>
2023-07-14 02:10:03 -0700

    Add files via upload

---
+++ dowhileloop14.java
@@ -0,0 +1,13 @@
+package javaoplab;
+
+public class dowhileloop14 {
+	  public static void main(String args[]) { 
+		  int x = 1;  
+		  do {       
+			  System.out.print("value of x : " + x );   
+			  x++;   
+			  System.out.print("\n");   
+			  }
+		  while( x < 11 ); 
+		   } 
+}
