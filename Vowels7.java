c3731f91e11b74c053d81b3e61910d4e20e1d775
ashra325 <86569956+ashra325@users.noreply.github.com>
2023-07-14 00:41:53 -0700

    Add files via upload

---
+++ Vowels7.java
@@ -0,0 +1,16 @@
+package javaoplab;
+
+public class Vowels7 {
+		 
+	    public static void main(String[] args) { 
+	 
+	        char ch = 'i'; 
+	 
+	        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )      
+	        	System.out.println(ch + " is vowel");  
+	        else        
+	        	System.out.println(ch + " is consonant"); 
+	 
+	    } 
+	}
+
